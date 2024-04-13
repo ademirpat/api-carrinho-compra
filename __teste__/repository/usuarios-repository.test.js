c16c56282cf3d045ad2f52812d8dd8de576543bf
Francisco Wallison <franciscowallison@gmail.com>
2024-04-13 09:01:33 -0300

    add teste U usuarios repository

---
+++ __teste__/repository/usuarios-repository.test.js
@@ -0,0 +1,24 @@
+const {
+  getUsuarios,
+  createUsuarios,
+} = require("../../repository/usuarios-repository");
+
+const countUsuarios = 5;
+
+test("get all usuarios", async () => {
+  const allUsuarios = await getUsuarios();
+  expect(allUsuarios.length).toBe(countUsuarios);
+});
+
+test("get all usuarios add new user", async () => {
+  const usuario = {
+    nome: "teste 23456",
+    email: "dfghjkliiiii@gmail.com",
+    senha: "123456",
+  };
+  await createUsuarios(usuario);
+
+  const allUsuarios = await getUsuarios();
+
+  expect(allUsuarios.length).toBe(countUsuarios);
+});
