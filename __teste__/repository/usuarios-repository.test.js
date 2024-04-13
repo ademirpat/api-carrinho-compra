b1bd606172c429bc4fafc3da234eee1d68df3838
Francisco Wallison <franciscowallison@gmail.com>
2024-04-13 11:08:17 -0300

    add logica para novo usuario em teste

--- __teste__/repository/usuarios-repository.test.js
+++ __teste__/repository/usuarios-repository.test.js
@@ -1,6 +1,7 @@
 const {
   getUsuarios,
   createUsuarios,
+  findUsuario
 } = require("../../repository/usuarios-repository");
 
 const countUsuarios = 5;
@@ -13,7 +14,7 @@ test("get all usuarios", async () => {
 test("get all usuarios add new user", async () => {
   const usuario = {
     nome: "teste 23456",
-    email: "dfghjkliiiii@gmail.com",
+    email: "teste23456@gmail.com",
     senha: "123456",
   };
   await createUsuarios(usuario);
@@ -21,4 +22,16 @@ test("get all usuarios add new user", async () => {
   const allUsuarios = await getUsuarios();
 
   expect(allUsuarios.length).toBe(6);
+});
+
+
+test("get new user", async () => {
+  const usuario = {
+    nome: "teste 23456",
+    email: "teste23456@gmail.com",
+    senha: "123456",
+  };
+  const usuario_result = await findUsuario(usuario.email);
+
+  expect(usuario_result.nome.toLowerCase()).toBe(usuario.nome.toLowerCase());
 });
--- models/index.js
+++ models/index.js
@@ -8,10 +8,6 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 
-console.log('====================================');
-console.log(process.env.NODE_ENV, "TESSSSSSSSSSSSSSSSS");
-console.log('====================================');
-
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
